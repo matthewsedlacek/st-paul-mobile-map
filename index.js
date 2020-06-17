265864f2c13160f3046d1bd23faab31e79fa9103
github username <github email address>
2020-06-17 11:13:31 -0700

    adds message feature with scroll

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const form = document.querySelector(".message-form")
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log('DOM fully loaded and parsed')
     getBikeTrailData(2)
-    // getMessage()
+    getMessage()
 })
 
 
@@ -71,50 +71,86 @@ function getMessage(){
 }
 
 function renderMessage(message){
-    const div = document.createElement("div")
-    div.className = "msgCard"
-    div.hidden = true
+    const cardBlock = document.querySelector(".card-block")
+    // const cardBlock = document.createElement("div")
+    // cardBlock.className = "card-block"
     
-    const user_name = document.createElement("li")
-    user_name.innerText = message.user_name
-
-    const msg = document.createElement("p")
-    msg.innerText = message.content
-
-    const bikeTrail = document.createElement("li")
-    bikeTrail.id = message.bike_trail_id
-    bikeTrail.innerText = message.bike_trail_id
-    bikeTrailListener(bikeTrail, div)
+    const card = document.createElement("div")
+    card.className = "card"
+    // card.className = `msg-${message.bike_trail_id}`
+    card.id = `msg-${message.bike_trail_id}`
+    card.hidden = true
     
-    msgSection.appendChild(messages)
-    div.append(bikeTrail, user_name, msg)
-    messages.appendChild(div)
-    msgTabs.append(bikeTrail)
+    const row = document.createElement("div")
+    row.className = "row"
+    
+    const userCol = document.createElement("div")
+    userCol.className = "col-sm-4"
+    userCol.innerText = message.user_name
+    
+    const msgCol = document.createElement("div")
+    msgCol.className = "col-sm-6"
+    msgCol.innerText = message.content
+
+    
+    row.append(userCol, msgCol)
+    card.appendChild(row)
+    cardBlock.appendChild(card)
+    // console.log(cardBlock)
+
+    // const div = document.createElement("div")
+    // div.className = "row"
+    // div.hidden = true
+
+    // const userCol = document.querySelector(".col-sm-4")
+    // const cardText = document.querySelector(".card-text")
+    // // div.id = `message.bike_trail_id`
+    
+    // const user_name = document.createElement("li")
+    // user_name.innerText = message.user_name
+
+    // const msg = document.createElement("p")
+    // msg.innerText = message.content
+
+    // // const bikeTrail = document.createElement("li")
+    // // bikeTrail.id = message.bike_trail_id
+    // // bikeTrail.innerText = message.bike_trail.name
+    // // bikeTrailListener(bikeTrail, div)
+
+    // userCol.appendChild(user_name)
+    // cardText.appendChild(msg)
+    
+    // msgSection.appendChild(messages)
+    // div.append(user_name, msg)
+    // row.appendChild(div)
+
+    // messages.appendChild(div)
+    // msgTabs.append(bikeTrail)
 }
 
-function bikeTrailListener(bikeTrail, div) {
-    bikeTrail.addEventListener("click", (e) => {
-        const cards = document.querySelectorAll(".msgCard")
-        cards.forEach(card => card.hidden = true)
-        div.hidden = false
+// function bikeTrailListener(bikeTrail, div) {
+//     bikeTrail.addEventListener("click", (e) => {
+//         const cards = document.querySelectorAll(".msgCard")
+//         cards.forEach(card => card.hidden = true)
+//         div.hidden = false
 
-        const hiddenInput = document.querySelector(".message-form").childNodes[1]
-        const trailInput = document.createElement("input")
-        trailInput.type="hidden"
-        trailInput.id= `${bikeTrail.id}`
-        trailInput.name= `${bikeTrail.id}`
-        form.replaceChild(trailInput, hiddenInput)
-        addMessage(bikeTrail)
-    })
-}
+//         const hiddenInput = document.querySelector(".message-form").childNodes[1]
+//         const trailInput = document.createElement("input")
+//         trailInput.type="hidden"
+//         trailInput.id= `${bikeTrail.id}`
+//         trailInput.name= `${bikeTrail.id}`
+//         form.replaceChild(trailInput, hiddenInput)
+//         addMessage(bikeTrail)
+//     })
+// }
 
-function addMessage(bikeTrail){
+function addMessage(item){
     const userInput = document.querySelector(".user_name-input")
     const msgInput = document.querySelector(".message-input")
     const postBtn = document.querySelector(".message-button")
-    const newUser = document.createElement("li")
+    const newUser = document.createElement("p")
     const newMsg = document.createElement("p")
-    const newTrail = bikeTrail.id
+    const newTrail = parseInt(`${item.id}`)
 
     postBtn.addEventListener("click", (e) => {
         e.preventDefault()
@@ -131,7 +167,7 @@ function postMessage(newUser, newMsg, newTrail){
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Accept: "applicaiton/json "
+            Accept: "application/json "
         },
         body: JSON.stringify({
             "user_name": newUser.innerText,
@@ -139,8 +175,8 @@ function postMessage(newUser, newMsg, newTrail){
             "bike_trail_id": newTrail
         })
     })
-    // .then(resp => resp.json())
-    // .then(json => console.log(json))
+    .then(resp => resp.json())
+    .then(json => renderMessage(json))
 }
 
 // TRAIL DATA
@@ -234,6 +270,17 @@ function addListenerToDropdownItem(item) {
         card.hidden = false
         const chart = document.getElementById(`chartContainer-${e.target.id}`)
         chart.hidden = false
+        const messages = document.querySelectorAll(`#msg-${e.target.id}`)
+        messages.forEach( message => {
+            message.hidden = false
+        })
+
+        const hiddenInput = document.querySelector(".message-form").childNodes[1]
+        const trailInput = document.createElement("input")
+        trailInput.type="hidden"
+        trailInput.id= `${item.id}`
+        // trailInput.name= `${item.id}`
+        form.replaceChild(trailInput, hiddenInput)
+        addMessage(item)
     });
 }
-
